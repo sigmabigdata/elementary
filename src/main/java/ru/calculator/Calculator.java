0c7a5a430169bab6e645b7458a5d5e1f91f22cc6
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-03 10:28:22 +0300

    1.3 Арифметическте операции

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         int two = 2;
         int six = 6;
         int four = 4;
-        int five = 5:
+        int five = 5;
         int sixDivTwo = six/two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
