4767bc387f0742471a9fd2457ec8aa7c0f1b3fc2
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-03 11:47:00 +0300

    1.5 Методы

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,25 +1,18 @@
 package ru.calculator;
 
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six/two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        int onePlusTwo = one + two;
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        //System.out.println(sixDivTwo);
-        //System.out.println(fiveMinusTwo);
-        //System.out.println(fourTimeTwo);
-        //System.out.println(onePlusTwo);
-        System.out.println(size);
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
     }
 }
+
 
 
