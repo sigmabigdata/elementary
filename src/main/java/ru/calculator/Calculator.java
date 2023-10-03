c9e8cd541380366a49cc7c5b846662b2d385595a
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-03 12:56:51 +0300

    1.3 Арифметические операции

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,18 +1,21 @@
 package ru.calculator;
 
 public class Calculator {
-
-    public static void plus(int first, int second) {
-        int result = first + second;
-        System.out.println(result);
-    }
-
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six/two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        int onePlusTwo = one + two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+        System.out.println(onePlusTwo);
     }
 }
-
 
 
