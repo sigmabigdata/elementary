9e4bd1d0bb66e1cfafe6f7d849115d397c6ef1c4
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-11 16:38:45 +0300

    2.3 Входные данные

--- src/test/java/ru/condition/X2Test.java
+++ src/test/java/ru/condition/X2Test.java
@@ -17,8 +17,19 @@ class X2Test {
     }
 
     @Test
+    void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl).isEqualTo(expected);
+    }
+
+    @Test
     void whenA0B1C1X1Then2() {
-        int a = 0;
+        int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
@@ -29,9 +40,9 @@ class X2Test {
 
     @Test
     void whenA1B1C0X1Then2() {
-        int a = 0;
+        int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
@@ -42,10 +53,11 @@ class X2Test {
     void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
-        int c = 1;
-        int x = 0;
-        int expected = 2;
+        int c = 0;
+        int x = 1;
+        int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
+
     }
 }
