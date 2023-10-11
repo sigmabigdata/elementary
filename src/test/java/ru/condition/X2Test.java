61472859ef8eac9267aaf438729c5392323a42a0
anatoli dyrul <dyrulan@gmail.com>
2023-10-11 18:38:47 +0300

    2.3 Входные данные

--- src/test/java/ru/condition/X2Test.java
+++ src/test/java/ru/condition/X2Test.java
@@ -29,7 +29,7 @@ class X2Test {
 
     @Test
     void whenA0B1C1X1Then2() {
-        int a = 1;
+        int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
@@ -53,8 +53,8 @@ class X2Test {
     void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
-        int c = 0;
-        int x = 1;
+        int c = 1;
+        int x = 0;
         int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
