b5b4514ee989362169be87e38b27d03980716052
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-11 16:17:50 +0300

    2.2 Что такое импорт

--- src/main/java/ru/math/MathFunction.java
+++ src/main/java/ru/math/MathFunction.java
@@ -1,8 +1,4 @@
 package ru.math;
-import ru.math.*;
-import ru.calculator.*;
-import ru.condition.*;
-import ru.converter.*;
 
 public class MathFunction {
 
@@ -14,19 +10,11 @@ public class MathFunction {
         return first * second;
     }
 
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-    }
-
-    public static double difference (double first, double second){
+    public static double difference(double first, double second) {
         return first - second;
     }
-    public static double divide(double first, double second){
+
+    public static double divide(double first, double second) {
         return first / second;
     }
 
