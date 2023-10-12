1c0a4a4ad25fd4746e6317b2ea46cb2ad63fddbe
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-12 09:31:17 +0300

    2.6 Тесты для стороны прямоугольника

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP7K3Square2Dot29() {
+        float expected = 2.29f;
+        int p = 7;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP4K2Square0Dot88() {
+        float expected = 0.88f;
+        int p = 4;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
