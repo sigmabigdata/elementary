4eaca3eeb5f70c05b16b75280ad1fa1c8aa0783f
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-12 16:15:31 +0300

    3.1 Операторы ветвлений

---
+++ src/main/java/ru/condition/AlertDivByZero.java
@@ -0,0 +1,19 @@
+package ru.condition;
+
+public class AlertDivByZero {
+    public static void possibleDiv(int number) {
+        boolean resultZer = number == 0;
+        boolean resultNeg = number < 0;
+        if (resultZer) {
+            System.out.println("Could not div by 0.");
+        }
+        if (resultNeg){
+            System.out.println("This is negative number");
+        }
+    }
+    public static void main(String[] args) {
+        AlertDivByZero.possibleDiv(4);
+        AlertDivByZero.possibleDiv(0);
+        AlertDivByZero.possibleDiv(-1);
+    }
+}
