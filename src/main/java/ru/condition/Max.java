6f4a69bc11ecfc43e16edbd4f060f9b7f363a5e1
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-13 09:37:48 +0300

    3.7 Тернарное сравнение

---
+++ src/main/java/ru/condition/Max.java
@@ -0,0 +1,16 @@
+package ru.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        if (left == right){
+            return left;
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        int max = max(5,5);
+        System.out.println(max);
+    }
+}
