f4fe61f749e5d8cbf0a3c2bfcafa00ea160b84b6
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-13 12:48:45 +0300

    3.13 Множественное логическое выражение

---
+++ src/test/java/ru/condition/TriangleTest.java
@@ -0,0 +1,24 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TriangleTest {
+    @Test
+    void whenExist() {
+        double ab = 2.0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void notExist() {
+        double ab = 55.0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        assertThat(result).isFalse();
+    }
+}
