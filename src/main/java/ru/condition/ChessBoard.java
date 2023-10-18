1f38d4367070e5e08a73a094b92c6882db2eb5cc
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-18 15:28:47 +0300

    3.14 Math.abs

--- src/main/java/ru/condition/ChessBoard.java
+++ src/main/java/ru/condition/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if ((x1 - y1) == (x2 - y2) || (x1 + y1) == (x2 + y2)) {
+            if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
                 rsl = Math.abs(x2 - x1);
                 rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
             }
@@ -15,6 +15,7 @@ public class ChessBoard {
     }
 
     private static boolean isValid(int coordinate) {
+
         return coordinate >= 0 && coordinate <= 7;
     }
 }
