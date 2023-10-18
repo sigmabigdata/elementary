f43ba322a550dd6168581428ba1ae72c0679cb4a
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-18 17:01:42 +0300

    Промежуточный

---
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -0,0 +1,11 @@
+package ru.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        for (int i = 2; i <= number; i++) {
+            if (number % number ==0)
+        }
+    }
+}
+
+
