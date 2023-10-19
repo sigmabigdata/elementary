9176cf979c1bfa76e6d08c1a1454cb17a879d427
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-19 11:32:00 +0300

    4.8 Простое число_оператор прерывания

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -1,11 +1,14 @@
-/*package ru.loop;
+package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        for (int i = 2; i <= number; i++) {
-            if (number % number ==0)
+        boolean prime = number != 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
         }
+        return prime;
     }
-}*/
-
-
+}
