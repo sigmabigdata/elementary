3021cfd626974e469e27b9d40852015d365480de
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-26 16:06:27 +0300

    4.12 Крест в псевдографике

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -1,18 +1,14 @@
-/*package ru.loop;
-
-public class Mortgage {
-    public static int year(double amount, int salary, double percent) {
-        int year = 0;
-        double balance = 0;
-        double remains = amount;
-        double yearlyPayment = amount + (amount * (percent / 100));
-        while (amount >= 0) {
-            amount = salary - yearlyPayment;
-            balance =
-
-            year++;
-
-        }
-        return year;
-    }
-}*/
+//package ru.loop;
+//
+//public class Mortgage {
+//    public static int year(double amount, double salary, double percent) {
+//        int year = 0;
+//        while (amount > 0) {
+//            double yearlyPayment = amount + (amount * percent / 100);
+//            amount = amount - yearlyPayment;
+//            salary = salary - yearlyPayment;
+//            year++;
+//        }
+//        return year;
+//    }
+//}
