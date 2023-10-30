0eae937bc2b99080e67995ef02d6001933fa1e02
anatoli dyrul <dyrulan@gmail.com>
2023-10-30 21:53:40 +0300

    5.12 Перевернуть массив

--- src/main/java/ru/array/Turn.java
+++ src/main/java/ru/array/Turn.java
@@ -2,10 +2,11 @@ package ru.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int temp = array[0];
-        array[0] = array[array.length - 1];
-        array[array.length - 1] = temp;
+
         for (int index = 1; index < array.length - index - 1; index++) {
+            int temp = array[0];
+            array[0] = array[array.length - 1];
+            array[array.length - 1] = temp;
             temp = array[index];
             array[index] = array[array.length - index - 1];
             array[array.length - index - 1] = temp;
