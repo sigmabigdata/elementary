ea7cbf03342cae0d6bbb688eb022732327fcdc47
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-30 09:01:35 +0300

    5.2 Размер массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Длинна массива ages " + ages.length);
+        System.out.println("Длинна массива surnames " + surnames.length);
+        System.out.println("Длинна массива prices " + prices.length);
     }
 }
