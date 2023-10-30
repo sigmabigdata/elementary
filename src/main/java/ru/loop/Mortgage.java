1b21a958be03de485adacc8130f0d562fb0177b7
Sigmabigdata <sigmabigdata@gmail.com>
2023-10-30 08:47:18 +0300

    4.10 Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -1,14 +1,13 @@
-//package ru.loop;
-//
-//public class Mortgage {
-//    public static int year(double amount, double salary, double percent) {
-//        int year = 0;
-//        while (amount > 0) {
-//            double yearlyPayment = amount + (amount * percent / 100);
-//            amount = amount - yearlyPayment;
-//            salary = salary - yearlyPayment;
-//            year++;
-//        }
-//        return year;
-//    }
-//}
+package ru.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount = amount + (amount * percent / 100);
+            amount = amount - salary;
+            year++;
+        }
+        return year;
+    }
+}
