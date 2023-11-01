857806601191005d0248d508033d650651d80d32
Sigmabigdata <sigmabigdata@gmail.com>
2023-11-01 14:22:26 +0300

    5.20 Поиск минимума в диапазоне

---
+++ src/main/java/ru/array/MinDiapason.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+                for (int i = start; i <= finish; i++) {
+                    if (array[i] < min) {
+                        min = array[i];
+                    }
+          }
+        return min;
+    }
+}
