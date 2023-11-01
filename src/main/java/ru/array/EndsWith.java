c9eac73ba219ed00dc6559b6ddf722df70f658b1
Sigmabigdata <sigmabigdata@gmail.com>
2023-11-01 12:08:24 +0300

    5.17 Слово заканчивается на...

---
+++ src/main/java/ru/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = post.length - 1; i < post.length; i--) {
+            if (post[post.length - i - 1] != word[word.length - i - 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
