a0a89a28f25e701a04560ec65484d622404baac9
anatoli dyrul <dyrulan@gmail.com>
2023-11-08 21:02:45 +0300

    5.26 Двухмерный массив

--- src/main/java/ru/array/Matrix.java
+++ src/main/java/ru/array/Matrix.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        for (int i = 0; i < table.length; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < table[i].length; j++) {
                 table[i][j] = (i + 1) * (j + 1);
             }
