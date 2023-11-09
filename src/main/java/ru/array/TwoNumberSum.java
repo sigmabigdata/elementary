c7e9cdd39714681544cfb8c6695fc353286262dc
Sigmabigdata <sigmabigdata@gmail.com>
2023-11-09 09:23:26 +0300

    5.31 Метод двух указателей

--- src/main/java/ru/array/TwoNumberSum.java
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -3,22 +3,21 @@ package ru.array;
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
-        int j = 0;
-        while (j < array.length) {
-            if (array[i] + array[j] == target) {
-                return new int[]{i, j};
-            }
+        int j = array.length - 1;
+        while (j > 0) {
             if (array[i] + array[j] != target) {
                 i++;
             }
-            j++;
+            if (array[i] + array[j] == target) {
+                return new int[]{i, j};
+            }
+            j--;
         }
+
         return new int[0];
     }
-}
 
-
-/*    public static int[] getIndexes(int[] array, int target) {
+    public static int[] getIndexes2(int[] array, int target) {
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] + array[j] == target) {
@@ -27,4 +26,5 @@ public class TwoNumberSum {
             }
         }
         return new int[0];
-    }*/
+    }
+}
