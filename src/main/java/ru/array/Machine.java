cdfdcec1be8c7a1158bdb0415334e5e5d1bb785d
Sigmabigdata <sigmabigdata@gmail.com>
2023-11-13 08:59:56 +0300

    5.32 Жадный алгоритм

---
+++ src/main/java/ru/array/Machine.java
@@ -0,0 +1,22 @@
+package ru.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        int change = money - price;
+        for (int coin: coins) {
+            while (change >= coin) {
+                rsl[size] = coin;
+                size++;
+                change -= coin;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
+
+
