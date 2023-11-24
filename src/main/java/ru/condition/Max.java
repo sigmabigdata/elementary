ddd73e893968c5b7377c87cfa682f34d939cbb39
Sigmabigdata <sigmabigdata@gmail.com>
2023-11-24 11:30:25 +0300

    7_15_Перегрузить_метод_max_для_трех_чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -2,12 +2,18 @@ package ru.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
+    public static int max(int first, int second, int third) {
+        return max(first, second) > third ? max(first, second) : third;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, second, third) > fourth ? max(first, second, third) : fourth;
+    }
     public static void main(String[] args) {
-        int max = max(5, 5);
+        int max = max(12, 9, 10, 11);
         System.out.println(max);
     }
 }
